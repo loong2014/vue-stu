fb11c119f091299a44ab752868cd85f665b033bb
zhangxin17 <zhangxin17@le.com>
2019-09-27 21:17:43 +0800

    添加element-ui

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,37 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 
+
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+const _import = require('./import-' + process.env.NODE_ENV)
+
+// 全局路由
+const globalRoutes = [
+  {
+    path: '/404',
+    component: _import('common/404'),
+    name: '404',
+    meta: {
+      title: '404未找到'
     }
-  ]
+  },
+  {
+    path: '/login',
+    component: _import('common/login'),
+    name: 'login',
+    meta: {
+      title: '登录'
+    }
+  }
+]
+
+const helloWorldRoute = {
+  path: '/',
+  name: 'HelloWorld',
+  component: HelloWorld
+}
+
+export default new Router({
+  routes: globalRoutes.concat(helloWorldRoute)
 })
---
+++ src/views/common/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>
+      <h2>400</h2>
+      <p>
+        抱歉！您访问的页面
+        <em>失联</em>啦 ...
+      </p>
+      <el-button @click="$router.go(-1)">返回上一页</el-button>
+      <el-button type="primary" @click="$router.push({ name: 'home' })">进入首页</el-button>
+    </div>
+  </div>
+</template>
